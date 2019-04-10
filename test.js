a69f90dd30eca2d98268e5c5741fa9642de56e2e
Aditya <adityasiwan007@gmail.com>
2019-04-10 16:17:57 +0530

    potato potato

---
+++ test.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+
+app.get('/igdg',async (req,res,next)=>{
+    let pro_time = "Mayank aaloo hai"
+    
+    res.send(pro_time);
+});
+
+
+app.get('/return/:data',async (req,res,next)=>{
+    let {data} = req.params
+    
+    let pro = `This is return valute to server: `+data;
+    res.send(pro);
+});
+app.use('/', express.static(__dirname + '/'));
+app.listen(4200);
+
